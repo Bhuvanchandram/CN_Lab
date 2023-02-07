88055df0ec8f1ab07e00be389377bb712d02d9a7
Bhuvanchandra M <bhuvanchandra668@gmail.com>
2023-02-07 14:30:58 +0530

    Changes

--- 01_framesort.py
+++ 01_framesort.py
@@ -4,23 +4,22 @@ class Frame:
         self.data=data
 
 def sort(frames):
-    n = len(frames)
+    n=len(frames)
     for i in range(n-1):
-        for j in range(n-i-1):
-            if frames[j].seqno > frames[j+1].seqno:
-                frames[j], frames[j+1] = frames[j+1], frames[j]
+        if(frames[i].seqno>frames[i+1].seqno):
+            frames[i],frames[i+1]=frames[i+1],frames[i]
 
 n=int(input("Enter the number of frames: "))
 
 frames = []
 
 for i in range(n):
-    seqno=int(input("Enter the sequence number: "))
+    seqno=int(input("Enter the sequence of Frame no {}: ".format(i+1)))
     data=int(input("Enter the data: "))
     frames.append(Frame(seqno,data))
 
 sort(frames)
 
-print("The sorted frame order is ")
+print("The sorted frames is: ")
 for i in range(n):
-    print("Seqno {} : Data {} ".format(frames[i].seqno,frames[i].data))
+    print("Seq no {} : Data {} ".format(frames[i].seqno,frames[i].data))
